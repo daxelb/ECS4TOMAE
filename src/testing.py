6db645cf227abb299462ddcbd3c805ab17f4b226
Damian Browne <damianabrowne@gmail.com>
2021-05-25 23:30:50 -0600

    Deleted testing py file

--- src/testing.py
+++
@@ -1,7 +0,0 @@
-from causalgraphicalmodels import CausalGraphicalModel
-
-takeone = CausalGraphicalModel(
-  nodes=["W","X","Y","Z"],
-  edges=[("W","X"), ("W","Y"), ("X","Z"), ("Z","Y")]
-)
-takeone.draw().render('test-output/test-model.gv', view=False)
