a830968a4bba050f9f00d8ef178a6d384d3b0650
Damian Browne <damianabrowne@gmail.com>
2021-07-05 23:22:23 -0600

    Deleted unused methods... Hopefully still able to get results as good as before!

--- src/knowledge.py
+++ src/knowledge.py
@@ -1,11 +1,9 @@
 from query import Quotient, Summation, Product, Queries, Query
 import util
 import gutil
-import random
 
-DIV_NODE_CONF = 0.06
+DIV_NODE_CONF = 0.025
 SAMPS_NEEDED = 25
-# DIV_EPS_DEC_SLOWNESS = 2
 
 class Knowledge():
   def __init__(self, environment, agent):
@@ -128,6 +126,10 @@ class KnowledgeAdjust(KnowledgeSensitive):
   def __init__(self, *args):
     super().__init__(*args) 
   
+  def get_num_datapoints(self, tf, other):
+    return len(gutil.only_dicts_with_givens(self.my_data(), tf[0].get_assignments(tf[0].e())))\
+      + len(gutil.only_dicts_with_givens(self.samples[other], tf[1].get_assignments(tf[1].e())))
+  
   def transport_formula(self, agent, givens):
     unformatted_tf = self.model.from_cpts(
         self.model.selection_diagram(
@@ -148,29 +150,6 @@ class KnowledgeAdjust(KnowledgeSensitive):
       else:
         other_queries.append(q)
     return Product([my_queries, other_queries]).assign(self.domains).assign(givens)
-  
-  def from_cpts(self, query, givens):
-    """
-    Returns the input query in terms
-    of the model's CPTs.
-    """
-    return Product([
-        q for q in
-        self.model.get_dist().assign(self.domains).assign(givens)
-        if q.query_var() not in givens and
-        q.contains(query.get_vars())
-      ])
-    
-  def formatted_transport_formula(self, agent, givens, action):
-    div_nodes = self.div_nodes(agent)
-    my_tf = Product()
-    other_tf = Product()
-    for q in self.from_cpts(self.transport_formula(agent, givens), {**action, **givens}):
-      if q.query_var() in div_nodes:
-        my_tf.append(q)
-        continue
-      other_tf.append(q)
-    return Product([my_tf, other_tf])
     
   def solve_transport_formula(self, f, my_data, other_data):
     f = f.deepcopy()
@@ -183,24 +162,6 @@ class KnowledgeAdjust(KnowledgeSensitive):
         return None
       summation += sol1 * sol2
     return summation
-  
-  def get_uncomputed_prob_of_tf(self, tf, my_data, other_data):
-    f = tf.deepcopy()
-    summation = [0,0]
-    for summator in gutil.permutations(f.get_unassigned()):
-      f.assign(summator)
-      my_prob = f[0].uncomputed_prob(my_data)
-      other_prob = f[1].uncomputed_prob(other_data)
-      if my_prob is None or other_prob is None:
-        return None
-      summation[0] += my_prob[0] * other_prob[0]
-      summation[1] += my_prob[1] * other_prob[1]
-    return summation
-
-  def get_num_datapoints(self, tf, other):
-    return len(gutil.only_dicts_with_givens(self.my_data(), tf[0].get_assignments(tf[0].e())))\
-      + len(gutil.only_dicts_with_givens(self.samples[other], tf[1].get_assignments(tf[1].e())))
-    
     
   def optimal_choice(self, givens={}):
     if len(self.my_data()) < SAMPS_NEEDED:
@@ -211,44 +172,29 @@ class KnowledgeAdjust(KnowledgeSensitive):
     my_data = self.my_data()
     
     action_rewards = {}
-    for action in actions:
-      act_hash = util.hash_from_dict(action)
+    for act in actions:
+      act_hash = util.hash_from_dict(act)
       action_rewards[act_hash] = {}
       for rew in rewards:
         action_rewards[act_hash][util.hash_from_dict(rew)] = [[],[]]
-        # action_rewards[act_hash][util.hash_from_dict(rew)] = [0,0]
     
     for other, other_data in self.samples.items():
       transport_formula = self.transport_formula(other, givens)
-      for action in actions:
-        transport_formula.assign(action)
-        act_hash = util.hash_from_dict(action)
-        
-        # this is only true for the model we are using with vars W,X,Y,Z and does not always hold.
-        # the formula should be: number of datapoints where conditionals are satisfied for each
-        # part of the transport formula.
-        # Ex: P*(A|B=1) * P(C|A) would be: num*(data B=1) + num(all data)
-        # num_datapoints = len(gutil.only_dicts_with_givens(data, givens)) + len(gutil.only_dicts_with_givens(my_data, action))
-        
+      for act in actions:
+        transport_formula.assign(act)
+        act_hash = util.hash_from_dict(act)
         num_datapoints = self.get_num_datapoints(transport_formula, other)
         if not num_datapoints:
           continue
         # calculating the transport formula with every action and every agent is slow
-        # transport_formula = self.formatted_transport_formula(agent, givens, action)
-        for reward in rewards:
-          rew_hash = util.hash_from_dict(reward)
-          transport_formula.assign(reward)
+        for rew in rewards:
+          transport_formula.assign(rew)
+          rew_hash = util.hash_from_dict(rew)
           tf_sol = self.solve_transport_formula(transport_formula, my_data, other_data)
           if tf_sol is None:
             continue
           action_rewards[act_hash][rew_hash][0].append(num_datapoints)
           action_rewards[act_hash][rew_hash][1].append(tf_sol)
-          # action_rewards[act_hash][0].append(gutil.first_value(reward) * tf_sol[0])
-          # action_rewards[act_hash][1] += tf_sol[1]
-    
-    # for act_key in action_rewards:
-    #   denom = action_rewards[act_key][1]
-    #   action_rewards[act_key] = sum(action_rewards[act_key][0]) / denom if denom else None
     
     weighted_act_rew = gutil.Counter()
     for act in action_rewards:
