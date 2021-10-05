82bf0c6cb2e5283ff1a7f7b2d74c4a73c000a44d
Damián Browne <damianabrowne@gmail.com>
2021-10-05 01:10:32 -0700

    value update!
    Former-commit-id: aa14fcd0056669ea1a91d395c52cfefb4597da00
    Former-commit-id: 0c721f3850fae51d2ad70c6ded51ca04d4fd6ef2
    Former-commit-id: 5c08c5f6d98fcf3dca7f33cd292377f13b678bcb

--- src/sim.py
+++ src/sim.py
@@ -262,18 +262,18 @@ if __name__ == "__main__":
   experiment = Sim(
     environment_dicts=(baseline, reversed_w, baseline, reversed_w),
     otp=(OTP.SOLO,OTP.NAIVE, OTP.SENSITIVE, OTP.ADJUST),
-    asr=ASR.TS,
-    T=100,
-    mc_sims=1,
+    asr=ASR.TEDS,
+    T=3000,
+    mc_sims=50,
     tau=0.05,
     EG_epsilon=0.02,
-    EF_rand_trials=30,
-    ED_cooling_rate=0.97,
+    EF_rand_trials=50,
+    ED_cooling_rate=0.98,
     is_community=True,
     rand_envs=True,
     node_mutation_chance=(0.2,0.8),
     show=True,
-    save=False,
+    save=True,
     seed=None
   )
-  experiment.run(desc="asrCommunity_T3000")
+  experiment.run(desc="otpED")
